5b54f8ae08efc94fe935357f1bab7ab3e4e11422
Andrew Law <andrewcylaw@hotmail.com>
2016-10-22 14:22:06 -0400

    website update

--- pave.js
+++ pave.js
@@ -4,8 +4,6 @@
 // App name space
 var pave = pave || {};
 
-
-
 // Embed a google map widget into the website
 pave.initMap = function() {
 	var uluru = {lat: -25.363, lng: 131.044};
@@ -19,6 +17,15 @@ pave.initMap = function() {
 	});
 }
 
+// Manipulate sidenav bar
+pave.openNav = function() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+pave.closeNav = function() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 // Init function
 pave.init = function() {
