781bebc78406e8e7a3fd8dbd3cd5ca29f8524860
Andrew Law <andrewcylaw@hotmail.com>
2016-10-22 16:35:56 -0400

    added submit button and data collection

--- pave.js
+++ pave.js
@@ -4,6 +4,11 @@
 // App name space
 var pave = pave || {};
 
+// User data entry
+pave.startAddress = "";
+pave.endAddress = "";
+pave.radius = 0;
+
 // Embed a google map widget into the website
 pave.initMap = function() {
 	var uluru = {lat: -25.363, lng: 131.044};
@@ -22,10 +27,18 @@ pave.openNav = function() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
 pave.closeNav = function() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// Submit form
+pave.enterPrefs = function(){
+	pave.startAddress = document.getElementById("startAddress").value;
+	pave.endAddress = document.getElementById("endAddress").value;
+	pave.radius = parseFloat(document.getElementById("radius").value);
+}
+
+
 
 // Init function
 pave.init = function() {
